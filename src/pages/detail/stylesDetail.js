93497f153ef35aa968347230b27aeec589e7e852
juancarlos301 <juancarlos.31.2002@gmail.com>
2023-01-28 15:09:40 -0500

    add-new-changes

--- src/pages/detail/stylesDetail.js
+++ src/pages/detail/stylesDetail.js
@@ -22,14 +22,13 @@ export const ContainerIcons = styled.div`
 `;
 
 export const Copy = styled.input`
-background-color: #f6f8fa;
+  background-color: #f6f8fa;
   border: 1px solid #d1d5da;
   border-radius: 3px;
   padding: 8px 12px;
   font-size: 14px;
   width: 80%;
   border-radius: 5px;
-}
 `;
 
 export const Divbutton = styled.div`
@@ -41,15 +40,14 @@ export const Divbutton = styled.div`
 `;
 
 export const CopieA = styled.button`
-display: flex;
-justify-content: end;
-color: black;
-margin-right: 20px;
-font-size: 14px;
-cursor: pointer;
-width: 100%;
-transition: 0.3s;
-}
+  display: flex;
+  justify-content: end;
+  color: black;
+  margin-right: 20px;
+  font-size: 14px;
+  cursor: pointer;
+  width: 100%;
+  transition: 0.3s;
 `;
 
 export const ContainerReturn = styled.div`
